75083aa3633066423216eaf60a26b16e194284c0
manish <manish@laptoplenovo>
2020-06-25 13:14:48 +0530

    useEffects

--- src/components/list.js
+++ src/components/list.js
@@ -1,15 +1,22 @@
 // src/components/list.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function List() {
     const [todoList, setTodo] = useState(["todo 1", "todo 2", "todo 3"])
 
     const addTodo = () => {
-        
-
         setTodo([...todoList, "todo " + (todoList.length + 1)])
     }
+
+    useEffect( () => {
+        document.title = "New Todo Added!"
+        window.localStorage.setItem("todoList", todoList)
+
+        return () => {
+            window.localStorage.clear()
+        }
+    })
     return (
         <div className="container-fluid">
             <div className="row">
