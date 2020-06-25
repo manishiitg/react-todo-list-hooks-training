dc75e7657b435d4e8806bdba2daed5bb7197ad49
manish <manish@laptoplenovo>
2020-06-25 12:13:22 +0530

    simple add todo

--- src/components/list.js
+++ src/components/list.js
@@ -3,19 +3,29 @@
 import React, { useState } from 'react';
 
 function List() {
-    const [todoList, setTodo] = useState(["todo1", "todo2", "todo3"])
+    const [todoList, setTodo] = useState(["todo 1", "todo 2", "todo 3"])
+
+    const addTodo = () => {
+        
+
+        setTodo([...todoList, "todo " + (todoList.length + 1)])
+    }
     return (
         <div className="container-fluid">
             <div className="row">
                 <ul className="list-group">
                     {
                         todoList.map((item, idx) => {
-                            return <li id={idx} className="list-group-item d-flex justify-content-between align-items-center">
+                            return <li key={idx} className="list-group-item d-flex justify-content-between align-items-center">
                                 {item}
-                                <span class="badge bg-danger rounded-pill">&times;</span>
+                                <span className="badge bg-danger rounded-pill">&times;</span>
                             </li>
                         })
                     }
+
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        <button className="btn btn-primary" onClick={addTodo}>Add Random Test Todo</button>
+                    </li>
 
                 </ul>
             </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,10 +4,10 @@ function Nav() {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand">TodoList</a>
 
-                <ul class="nav">
-                    <li class="nav-item">
+                <ul className="nav">
+                    <li className="nav-item">
                         <button type="button" className="btn btn-primary">Add Todo</button>
                     </li>
                 </ul>
