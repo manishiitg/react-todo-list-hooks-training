84d90b4831b0907e11696cfa0d27e06af29a8fb4
manish <manish@laptoplenovo>
2020-06-25 11:58:11 +0530

    todo list state

--- src/components/list.js
+++ src/components/list.js
@@ -1,20 +1,22 @@
-import React from 'react';
+// src/components/list.js
+
+import React, { useState } from 'react';
 
 function List() {
+    const [todoList, setTodo] = useState(["todo1", "todo2", "todo3"])
     return (
         <div className="container-fluid">
             <div className="row">
                 <ul className="list-group">
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                        Cras justo odio
-              <span class="badge bg-danger rounded-pill">
-                            &times;
-              </span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">Dapibus ac facilisis in</li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">Morbi leo risus</li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">Porta ac consectetur ac</li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">Vestibulum at eros</li>
+                    {
+                        todoList.map((item, idx) => {
+                            return <li id={idx} className="list-group-item d-flex justify-content-between align-items-center">
+                                {item}
+                                <span class="badge bg-danger rounded-pill">&times;</span>
+                            </li>
+                        })
+                    }
+
                 </ul>
             </div>
         </div>
